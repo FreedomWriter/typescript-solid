8c1fe50678e9c002e988007d5e0c33c7bc94171f
Natalie Davis <natalienetworks@gmail.com>
2023-01-13 09:43:28 -0600

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Hello World</Title>
+      <h1>Hello world!</h1>
+    </main>
+  );
+}
