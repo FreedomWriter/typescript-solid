9b2e98a6b94403cae971c4883384b874960dbb4d
Natalie Davis <natalienetworks@gmail.com>
2023-01-14 16:38:09 -0600

    NEW: basic transpiling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,56 @@
+import * as esbuild from "esbuild-wasm";
+import { Accessor, createEffect, createSignal, onMount } from "solid-js";
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
 
+const [ref, setRef] = createSignal<any>({ current: undefined });
 export default function Home() {
+  const [value, setValue] = createSignal("");
+  const [code, setCode] = createSignal("");
+
+  const startService: () => Promise<void> = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    setRef({ current: { ...service } });
+  };
+  const handleClick = async (): Promise<(() => () => void) | undefined> => {
+    await startService();
+    if (!ref()?.current) {
+      return;
+    }
+
+    const res = await ref()?.current.transform(value(), {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(res.code);
+  };
+
+  let inputRef!: HTMLTextAreaElement;
+  const handleChange: () => void = () => {
+    setValue(inputRef.value);
+  };
+
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Solid App</Title>
+      <section>
+        <textarea
+          onChange={(e) => console.log(e.target.textContent)}
+          id="userInput"
+          name="userInput"
+          ref={inputRef}
+          onInput={handleChange}
+        />
+        <div>
+          <button type="submit" onClick={handleClick}>
+            Submit
+          </button>
+        </div>
+        <pre>{code()}</pre>
+      </section>
     </main>
   );
 }
